31346e19e518ce3cf91431f340e9e889c50815e9
Jessica <seipje@gmail.com>
2014-04-16 00:56:38 +0000

    Change where images are stored

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,7 @@
 class Piece < ActiveRecord::Base
   belongs_to :artist
   
-  has_attached_file :photo, :styles => {:small => '150x150>' }
+  has_attached_file :photo, :styles => {:small => '150x150>' }, :url => "/assets/pieces/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/pieces/:id/:style/:basename.:extension"
   
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
